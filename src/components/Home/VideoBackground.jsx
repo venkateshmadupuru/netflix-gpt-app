1ad7476d9f69f8fdb8779146dd2f9fcb11dfdf8c
VenkateshEarth <venkat5041075@gmail.com>
2025-11-11 22:30:23 +0530

    fix(videoBackground): disable suggestions and loop trailer

--- src/components/Home/VideoBackground.jsx
+++ src/components/Home/VideoBackground.jsx
@@ -4,11 +4,12 @@ import useMovieTrailer from "../../hooks/useMovieTrailer";
 const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store.movies?.trailerVideo);
   useMovieTrailer(movieId);
+  if (!trailerVideo?.key) return null;
   return (
     <div>
       <iframe
         className="w-full aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1`}
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo.key}&modestbranding=1&rel=0&showinfo=0`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerPolicy="strict-origin-when-cross-origin"
