52361f0f37fcf4ec2f096a487e173e9088146e79
VenkateshEarth <venkat5041075@gmail.com>
2026-01-31 23:48:46 +0530

    refactor(validation): consolidate firstname and lastname into username validation

--- src/utils/Valid.js
+++ src/utils/Valid.js
@@ -1,8 +1,7 @@
-export const checkValidData = (email, password, firstname, lastname) => {
+export const checkValidData = (email, password, username) => {
     const isEmailValid =/^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
     const isPasswordValid =/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    const isFirstNameValid = /^[a-zA-Z\s]+$/.test(firstname);
-    const isLastNameValid = /^[a-zA-Z\s]+$/.test(lastname);
+    const isUserNameValid = /^[a-zA-Z\s]+$/.test(username);
 
     if (!isEmailValid) {
         return "Please enter a valid email address.";
@@ -10,11 +9,8 @@ export const checkValidData = (email, password, firstname, lastname) => {
     if (!isPasswordValid) {
         return "Password is not Valid, it should contain at least 8 characters, including one uppercase letter, one lowercase letter, and one number.";
     }
-    if (!isFirstNameValid) {
-        return "First name can only contain letters and spaces.";
-    }
-    if (!isLastNameValid) {
-        return "Last name can only contain letters and spaces.";
+    if (!isUserNameValid) {
+        return "Username can only contain letters and spaces.";
     }
     return null;
 };
